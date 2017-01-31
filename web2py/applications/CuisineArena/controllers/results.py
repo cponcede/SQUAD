bf66ab9493a293eb0a6bc67b3193e7b5f8ce46e2
Christopher Ponce de Leon <cponcede@rescomp-16-303152.stanford.edu>
2017-01-31 08:20:44 -0800

    Added TODOs to results.py controller

--- web2py/applications/CuisineArena/controllers/results.py
+++ web2py/applications/CuisineArena/controllers/results.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 # try something like
 def list():
+    # Inside of session variable should be a Dictionary<String, Double> that maps from cuisine name -> rating
+    # Inside of session should also be location and price preferences
+    # Pass all of the above information to resultsmodel.py to get back a list of restaurants and information about each.
+    # Return that dictionary to results/list.html, which will use this info to fill out the view.
     return dict(message="This is the view for viewing a list of restaurant results")
 
 def restaurant():
