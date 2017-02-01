92b67dee5e0ee03bebc4c38ac56ac22a1b0fccf0
Teddy Morris-Knower <teddymorris-knower@DN51ul7i.SUNet>
2017-01-31 17:19:09 -0800

    Change Welcome.py

--- web2py/applications/CuisineArena/controllers/welcome.py
+++ web2py/applications/CuisineArena/controllers/welcome.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 # try something like
 def landingpage(): return dict(message="This is the landing page for CuisineArena.")
+def randomNewPage(): return dict(message="TEST THIS")
