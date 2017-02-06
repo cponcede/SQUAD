e5640ee913f654d744835dd4745c6ff84005982a
Christopher Ponce de Leon <cponcede@rescomp-16-303152.stanford.edu>
2017-02-05 21:49:48 -0800

    Write the FactualSearch module, which performs all interactions with the Factual API for us. Currently, we still have no API key so the functions internally just hardcode results from the API and manipulate those results as if they were real

--- web2py/applications/CuisineArena/controllers/results.py
+++ web2py/applications/CuisineArena/controllers/results.py
@@ -1,17 +1,18 @@
-# factualAPI = factualAPI()
+from factualsearch import FactualSearch
+
+factualAPI = FactualSearch()
 
 def list():
     zipCode = session.zipCode
     maxDistance = session.maxDistance
     pricePrefs = session.pricePrefs
     cuisineRatings = session.cuisineRatings
-    # results = factualAPI.searchForRestaurants()
+    results = factualAPI.searchForRestaurants(session.zipCode, session.maxDistance, session.pricePrefs, session.cuisineRatings)
     # Return that dictionary to results/list.html, which will use this info to fill out the view.
-    return dict(message="This is the view for viewing a list of restaurant results")
+    return dict(message="This is the view for viewing a list of restaurant results", results=results)
 
 def restaurant():
-    # Message resultsmodel.py to get info on that restaurant
-    # Return response dicitonary from resultsmodel.py
-    # results/restaurant.html will use this to fill out the view with info about that restaurant
-    msg = "You are viewing results for " + request.vars['name']
-    return dict(message=msg)
+    restaurantId = request.vars.get('id', -1) # URL parameter is the factual ID of the chosen restaurant
+    restaurantInfo = factualAPI.getRestaurantDetails(restaurantId)
+    msg = "You are viewing results for " + restaurantInfo['name']
+    return dict(message=msg, details=restaurantInfo)
