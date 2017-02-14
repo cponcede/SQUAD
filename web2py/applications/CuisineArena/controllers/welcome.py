46db938b313484c09bab646ffa0ca8c8370e4f63
Teddy Morris-Knower <teddymorris-knower@rescomp-16-264083.stanford.edu>
2017-02-13 21:20:45 -0800

    Added person table, authentication to welcome controller, removed bottom navbar

--- web2py/applications/CuisineArena/controllers/welcome.py
+++ web2py/applications/CuisineArena/controllers/welcome.py
@@ -10,47 +10,42 @@ def landingpage():
 
 def signin():
     if request.vars.username:
-        #TODO: validate username and password
-        session.name = request.vars.username
-        session.cuisines = ["Indian","Italian","Mexican","Barbecue","Burgers","Chinese","Japanese","American_(New)","Pizza","Salad","Sandwiches","Seafood","Sushi","American_(Traditional)","Vietnamese"]
-        base_ELO = 1500
-        session.cuisineRatings = {cuisine:base_ELO for cuisine in session.cuisines}
-        session.previousCuisines = ["",""]
-        redirect(URL('welcome', 'preferences'))
+        user = db.person(db.person.username == request.vars.username)
+        if user:
+            if user.password == request.vars.password:
+                session.name = request.vars.username
+                session.pricePrefs = '1,2'
+                session.zipCode = '94305'
+                session.maxDistanceInMiles = 30
+                session.cuisines = ["Indian","Italian","Mexican","Asian_Fusion","Barbecue","Burgers","Chinese","Japanese","American_(New)","Pizza","Salad","Sandwiches","Seafood","Sushi","American_(Traditional)","Vietnamese"]
+                base_ELO = 1500
+                session.cuisineRatings = {cuisine:base_ELO for cuisine in session.cuisines}
+                session.previousCuisines = ["",""]
+                redirect(URL('cuisinearena', 'arena'))
+            else:
+                return dict(message='Password is incorrect', rows=db().select(db.person.ALL), user=user, password=user.password) #right now just returns to signin page
+        else:
+            return dict(message='No such user', rows=db().select(db.person.ALL)) #right now just returns to singin page
     else:
         return dict()
 
 def signup():
     if request.vars.username:
-        #TODO:store signup data in database
-        #TODO:error checking here or in HTML?
-        session.name = request.vars.username
-        session.cuisines = ["Indian","Italian","Mexican","Asian_Fusion","Barbecue","Burgers","Chinese","Japanese","American_(New)","Pizza","Salad","Sandwiches","Seafood","Sushi","American_(Traditional)","Vietnamese"]
-        base_ELO = 1500
-        session.cuisineRatings = {cuisine:base_ELO for cuisine in session.cuisines}
-        session.previousCuisines = ["",""]
-        redirect(URL('welcome', 'preferences'))
+        try:
+            db.person.insert(username=request.vars.username, password=request.vars.password)
+        except Exception, e:
+            db.rollback()
+            return dict(message='Invalid SignUp', exception = e, rows=db().select(db.person.ALL)) #right now this just returns to signup maybe should change
+        else:
+            db.commit()
+            session.name = request.vars.username
+            session.pricePrefs = {"$": True, "$$": False, "$$$": False, "$$$$": False}
+            session.zipCode = '94305'
+            session.maxDistanceInMiles = 30
+            session.cuisines = ["Indian","Italian","Mexican","Asian_Fusion","Barbecue","Burgers","Chinese","Japanese","American_(New)","Pizza","Salad","Sandwiches","Seafood","Sushi","American_(Traditional)","Vietnamese"]
+            base_ELO = 1500
+            session.cuisineRatings = {cuisine:base_ELO for cuisine in session.cuisines}
+            session.previousCuisines = ["",""]
+            redirect(URL('cuisinearena', 'arena'))
     else:
         return dict()
-
-def preferences():
-    if request.vars.price or request.vars.zipcode or request.vars.radius:
-        if request.vars.price:
-            if request.vars.zipcode:
-                if request.vars.radius:
-                    session.zipCode = request.vars.zipcode
-                    session.maxDistanceInMiles = float(request.vars.radius)
-                    priceString = ""
-                    for tier in request.vars.price:
-                        priceString = priceString + tier + ','
-                    priceString = priceString[:-1]
-                    session.pricePrefs = priceString
-                    redirect(URL('cuisinearena','arena'))
-                else:
-                    return {'error_msg': 'Error: No radius to search was provided.'}
-            else:
-                return {'error_msg': 'Error: No zipcode was provided'}
-        else:
-            return {'error_msg': 'Error: No price tiers were selected.'}
-    else:
-        return {'error_msg': ''}
