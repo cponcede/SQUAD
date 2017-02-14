76ab3840e1e26941410e4585d2eb016cadabcd9e
Christopher Ponce de Leon <cponcede@rescomp-16-303152.stanford.edu>
2017-02-13 19:59:38 -0800

    Removed old commented hard-coded values

--- web2py/applications/CuisineArena/controllers/welcome.py
+++ web2py/applications/CuisineArena/controllers/welcome.py
@@ -12,9 +12,6 @@ def signin():
     if request.vars.username:
         #TODO: validate username and password
         session.name = request.vars.username
-        #session.pricePrefs = '1,2'
-        #session.zipCode = '94305'
-        #session.maxDistanceInMiles = 30
         session.cuisines = ["Indian","Italian","Mexican","Barbecue","Burgers","Chinese","Japanese","American_(New)","Pizza","Salad","Sandwiches","Seafood","Sushi","American_(Traditional)","Vietnamese"]
         base_ELO = 1500
         session.cuisineRatings = {cuisine:base_ELO for cuisine in session.cuisines}
@@ -28,9 +25,6 @@ def signup():
         #TODO:store signup data in database
         #TODO:error checking here or in HTML?
         session.name = request.vars.username
-        #session.pricePrefs = {"$": True, "$$": False, "$$$": False, "$$$$": False}
-        #session.zipCode = '94305'
-        #session.maxDistanceInMiles = 30
         session.cuisines = ["Indian","Italian","Mexican","Asian_Fusion","Barbecue","Burgers","Chinese","Japanese","American_(New)","Pizza","Salad","Sandwiches","Seafood","Sushi","American_(Traditional)","Vietnamese"]
         base_ELO = 1500
         session.cuisineRatings = {cuisine:base_ELO for cuisine in session.cuisines}
