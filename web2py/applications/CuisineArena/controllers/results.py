e1ee5822be8b5097220107f8363aa8147772c967
Teddy Morris-Knower <teddymorris-knower@rescomp-16-264083.stanford.edu>
2017-03-17 18:08:59 -0700

    2nd half of group functionality, group check in bar

--- web2py/applications/CuisineArena/controllers/results.py
+++ web2py/applications/CuisineArena/controllers/results.py
@@ -2,14 +2,18 @@ from foursquareapi import FoursquareAPI
 foursquareAPI = FoursquareAPI()
 
 def list():
-    results = foursquareAPI.searchForRestaurants(session.zipCode,session.maxDistanceInMiles,
-        session.pricePrefs, session.finalRatings)
-    if len(results) == 0:
-        return dict(message="No results found. Try entering a different zip code or increasing the search radius.", searchResults=results, cuisineRatings=session.cuisineRatings)
-        
-    return dict(message="This is the view for viewing a list of restaurant results", searchResults=results, cuisineRatings=session.cuisineRatings)
+	results = foursquareAPI.searchForRestaurants(session.zipCode,session.maxDistanceInMiles, session.pricePrefs, session.finalRatings)
+	groupId = db((db.userGroup.username == session.name)).select().first().groupId
+	groupRows = db((db.userGroup.groupId == groupId)).select()
+	completedNames = [row.username for row in groupRows if row.completed]
+	inProgressNames = [row.username for row in groupRows if not row.completed]
+
+	if len(results) == 0:
+	    return dict(message="No results found. Try entering a different zip code or increasing the search radius.", searchResults=results, cuisineRatings=session.cuisineRatings, completed = completedNames, inProgress = inProgressNames, group=session.group)
+	else:
+		return dict(message="This is the view for viewing a list of restaurant results", searchResults=results, cuisineRatings=session.cuisineRatings, completed = completedNames, inProgress = inProgressNames, group=session.group)
 
 def restaurant():
-    restaurantId = request.vars.get('id', None)
-    restaurantInfo = foursquareAPI.getRestaurantDetails(restaurantId)
-    return dict(restaurantDetails=restaurantInfo)
+	restaurantId = request.vars.get('id', None)
+	restaurantInfo = foursquareAPI.getRestaurantDetails(restaurantId)
+	return dict(restaurantDetails=restaurantInfo)
