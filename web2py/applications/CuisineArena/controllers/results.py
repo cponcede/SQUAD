5644c4be6708e1ff61c9884ed0b6d074fdac9e3e
Christopher Ponce de Leon <cponcede@rescomp-16-303152.stanford.edu>
2017-03-23 15:26:27 -0700

    Fixed results for single user

--- web2py/applications/CuisineArena/controllers/results.py
+++ web2py/applications/CuisineArena/controllers/results.py
@@ -21,14 +21,16 @@ def list():
 	results = foursquareAPI.searchForRestaurants(session.zipCode,session.maxDistanceInMiles, session.pricePrefs, session.finalRatings)
 	priceTiers = session.pricePrefs.split(',')
 	results = foursquareAPI.searchForRestaurants(session.zipCode,session.maxDistanceInMiles, session.pricePrefs, session.finalRatings)
-	groupId = db((db.userGroup.username == session.name)).select().first().groupId
-	groupRows = db((db.userGroup.groupId == groupId)).select()
-	completedNames = [row.username for row in groupRows if row.completed]
-	inProgressNames = [row.username for row in groupRows if not row.completed]    =======
 	if len(results) == 0:
 	    return dict(message="No results found. Try entering a different zip code or increasing the search radius.", searchResults=results, cuisineRatings=session.cuisineRatings, completed = completedNames, inProgress = inProgressNames, group=session.group)
-	else:
+	if session.group:
+		groupId = db((db.userGroup.username == session.name)).select().first().groupId
+		groupRows = db((db.userGroup.groupId == groupId)).select()
+		completedNames = [row.username for row in groupRows if row.completed]
+		inProgressNames = [row.username for row in groupRows if not row.completed]
 		return dict(message="This is the view for viewing a list of restaurant results", searchResults=results, cuisineRatings=session.cuisineRatings, completed = completedNames, inProgress = inProgressNames, group=session.group)
+	else:
+		return dict(message="This is the view for viewing a list of restaurant results", priceTiers=priceTiers, searchResults=results, cuisineRatings=session.cuisineRatings, group=session.group, completed = [], inProgress = [])
 
 def restaurant():
 	restaurantId = request.vars.get('id', None)
